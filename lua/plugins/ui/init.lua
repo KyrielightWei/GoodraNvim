9f18e5aab6918e4f9c7bedf0f8135dd244545037
snorlaxrin <snorlaxrin@gmail.com>
2023-11-01 13:11:02 +0000

    new indent-blankline config

--- lua/plugins/ui/init.lua
+++ lua/plugins/ui/init.lua
@@ -65,12 +65,17 @@ return {
 
     {
         "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
         opts = {
             -- char = "▏",
             -- char = "│",
-            filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-            show_trailing_blankline_indent = false,
-            show_current_context = true,
+            -- filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+            exclude = {
+                filetypes = {"help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy"  },
+                buftypes = { "terminal" },
+            }
+            -- show_trailing_blankline_indent = false,
+            -- show_current_context = true,
         },
     },
     -- {
